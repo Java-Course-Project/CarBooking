d3af91e4ad2e3de91990bf0b2ea0bac581a0eddd
duyvu <duy.vu@aeris.net>
2024-12-10 09:39:34 +0700

    set priority

--- src/main/java/org/duyvu/carbooking/message/MessageTransfer.java
+++ src/main/java/org/duyvu/carbooking/message/MessageTransfer.java
@@ -1,15 +1,16 @@
 package org.duyvu.carbooking.message;
 
-import jakarta.jms.JMSException;
+import jakarta.jms.DeliveryMode;
+import jakarta.jms.MessageConsumer;
+import jakarta.jms.MessageProducer;
+import jakarta.jms.ObjectMessage;
+import jakarta.jms.Queue;
 import jakarta.validation.Valid;
 import java.time.Duration;
-import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.duyvu.carbooking.model.Message;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessagePostProcessor;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -31,27 +32,25 @@ public class MessageTransfer {
 
 	public void send(Topic topic, @Valid Message<?> msg) {
 		final long ttl = 30000L;
-		jmsTemplate.convertAndSend(topic.value, msg, new MessagePostProcessor() {
-			@Override
-			@NonNull
-			public jakarta.jms.Message postProcessMessage(@NonNull jakarta.jms.Message message) throws JMSException {
-				message.setJMSPriority(msg.getPriority());
-				message.setJMSExpiration(System.currentTimeMillis() + ttl);
-				message.setStringProperty("x_custom_id",msg.getId().toString());
-				return message;
-			}
+		jmsTemplate.execute(session -> {
+			Queue queue = session.createQueue(topic.value);
+			MessageProducer producer = session.createProducer(queue);
+			ObjectMessage message = session.createObjectMessage(msg);
+
+			// Set custom headers
+			message.setStringProperty("x_customer_id", msg.getId().toString());
+			producer.send(message, DeliveryMode.PERSISTENT, msg.getPriority(), ttl);
+			return null;
 		});
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> Message<T> receive(Topic topic, String selector, Duration timeout) {
-		Instant start = Instant.now();
-		while (Duration.between(start, Instant.now()).compareTo(timeout) < 0) {
-			Message<T> message = (Message<T>) jmsTemplate.receiveSelectedAndConvert(topic.value, selector);
-			if (message != null) {
-				return message;
-			}
-		}
-		return null;
+		return (Message<T>) jmsTemplate.execute(session -> {
+			Queue queue = session.createQueue(topic.value);
+			MessageConsumer consumer = session.createConsumer(queue, selector);
+
+			return consumer.receive(timeout.getSeconds()).getBody(Message.class);
+		});
 	}
 }
